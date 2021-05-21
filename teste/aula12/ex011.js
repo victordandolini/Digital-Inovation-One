1da54501faf3cdc27463385be23e750f02f6bbc3
VICTOR DANDOLINI <victordandolini@hotmail.com>
2021-05-21 14:49:11 -0300

    aula12

---
+++ teste/aula12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 67
+if (idade < 16) {
+console.log('Não vota')
+} else if (idade <  18 || idade > 65) {
+     console.log('voto opcional')
+} else {
+     console.log('voto obrigatorio')
+} 
