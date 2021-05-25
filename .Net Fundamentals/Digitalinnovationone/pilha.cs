2efdcca99fd1e9ad3c570758d06bd43eb6d5b393
VICTOR DANDOLINI <victordandolini@hotmail.com>
2021-05-24 21:27:40 -0300

    .net

---
+++ .Net Fundamentals/Digitalinnovationone/pilha.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EstruturaDoPrograma.Exemplos
+{
+    class pilha
+    {
+        posicao primeiro;
+
+        public void Empilha(object item)
+        {
+                primeiro = new posicao(primeiro, item);
+        }
+        public object Desempilha()
+        {
+            if (primeiro == null)
+            {
+                throw new InvalidOperationException("A pilha esta vazia");
+            }
+            object resultado = primeiro.item;
+            primeiro = primeiro.proximo;
+            return resultado;   
+        }
+        class posicao
+        {
+            public posicao proximo;
+            public object item;
+            public posicao(posicao proximo, object item)
+            {
+                    this.proximo = proximo;
+                    this.item = item;
+
+            }
+        }
+    }
+}
+
---
+++ .Net Fundamentals/index.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>Meu Primeiro PHP!</title>
+</head>
+
+<body>
+        <?php
+            for ( $i = 0 ; $i < 10 ; $i++ ){
+                print ("Linha número " . $i . "<br />");
+            }
+            ?>
+</body>
+</html> 
