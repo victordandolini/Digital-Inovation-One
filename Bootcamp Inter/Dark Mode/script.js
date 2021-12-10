22b71c1624b971de3ed35e3143efbb9c424193f1
VICTOR DANDOLINI <victordandolini@hotmail.com>
2021-12-10 00:41:53 -0300

    exercicios darkmode

---
+++ Bootcamp Inter/Dark Mode/script.js
@@ -0,0 +1,26 @@
+const BTN_DARK_MODE = document.querySelector("#btn-dark-mode")
+const LIGHT_MODE = document.querySelector("#light-mode")
+const BODY = document.getElementsByTagName("body")[0]
+
+BTN_DARK_MODE.addEventListener('click', toggleDarkMode)
+
+function toggleDarkMode(){
+    changeClass()
+    changeText()
+    
+}
+
+function changeClass(){
+    BTN_DARK_MODE.classList.toggle("dark-mode")
+    LIGHT_MODE.classList.toggle("dark-mode")
+    BODY.classList.toggle("dark-mode")
+}
+
+function changeText(){
+    if(LIGHT_MODE.classList.contains("dark-mode")){
+        LIGHT_MODE.innerHTML = "Dark Mode ON"
+    return
+    }
+    LIGHT_MODE.innerHTML = "Light Mode ON"
+
+}
