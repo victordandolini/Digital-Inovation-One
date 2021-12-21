ae2d1fa02973e2fe6f87eb689975b951ef294bb2
VICTOR DANDOLINI <victordandolini@hotmail.com>
2021-12-21 09:57:32 -0300

    exercicios

--- Bootcamp Inter/Dark Mode/script.js
+++ Bootcamp Inter/Dark Mode/script.js
@@ -1,26 +1,57 @@
-const BTN_DARK_MODE = document.querySelector("#btn-dark-mode")
-const LIGHT_MODE = document.querySelector("#light-mode")
-const BODY = document.getElementsByTagName("body")[0]
+// const BTN_DARK_MODE = document.querySelector("#btn-dark-mode")
+// const LIGHT_MODE = document.querySelector("#light-mode")
+// const BODY = document.getElementsByTagName("body")[0]
 
-BTN_DARK_MODE.addEventListener('click', toggleDarkMode)
+// BTN_DARK_MODE.addEventListener('click', toggleDarkMode)
 
-function toggleDarkMode(){
-    changeClass()
-    changeText()
+// function toggleDarkMode(){
+//     changeClass()
+//     changeText()
     
-}
+// }
 
-function changeClass(){
-    BTN_DARK_MODE.classList.toggle("dark-mode")
-    LIGHT_MODE.classList.toggle("dark-mode")
-    BODY.classList.toggle("dark-mode")
-}
+// function changeClass(){
+//     BTN_DARK_MODE.classList.toggle("dark-mode")
+//     LIGHT_MODE.classList.toggle("dark-mode")
+//     BODY.classList.toggle("dark-mode")
+// }
 
-function changeText(){
-    if(LIGHT_MODE.classList.contains("dark-mode")){
-        LIGHT_MODE.innerHTML = "Dark Mode ON"
-    return
-    }
-    LIGHT_MODE.innerHTML = "Light Mode ON"
+// function changeText(){
+//     if(LIGHT_MODE.classList.contains("dark-mode")){
+//         LIGHT_MODE.innerHTML = "Dark Mode ON"
+//     return
+//     }
+//     LIGHT_MODE.innerHTML = "Light Mode ON"
 
-}
+// }
+
+
+
+let n = 31
+let string ="LIFE IS NOT A PROBLEM TO BE SOLVED"
+let resultado = string.split('').slice(0,n).join('')
+console.log(resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
