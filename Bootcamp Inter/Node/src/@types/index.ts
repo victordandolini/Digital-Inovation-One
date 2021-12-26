5435b2291177567c054d403687953d6932d25551
VICTOR DANDOLINI <victordandolini@hotmail.com>
2021-12-26 03:20:47 -0300

    Add Typescrip and Developer  Modules

--- Bootcamp Inter/Node/src/@types/index.ts
+++ Bootcamp Inter/Node/src/@types/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello Word!")
+import express, {Request, Response, NextFunction } from 'express'
+const app = express()
+const port = 3000
+
+app.get('/status', (req:Request, res:Response, next: NextFunction) => {
+    res.status(200).send({foo: 'sucess'});
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+});
